d20a380cd9e86df61e733274fc564ee11de66bdd
Angel Gustavo Garza Aguilar <angel_winy@live.com.mx>
2022-09-08 17:41:23 -0500

    Initial commit

---
+++ SpeechText.Android/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Widget;
+using Android.Speech;
+using Android.Speech.Tts;
+using Android.Content;
+using System.Runtime.Remoting.Contexts;
+
+namespace SpeechText.Droid
+{
+    [Activity(Label = "SpeechText", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        private readonly int VOICE = 10;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultVal, Intent data)
+        {
+            if (requestCode == VOICE)
+            {
+                if (resultVal == Result.Ok)
+                {
+                    var matches = data.GetStringArrayListExtra(RecognizerIntent.ExtraResults);
+                    if (matches.Count != 0)
+                    {
+                        string textInput = matches[0];
+
+                        // limit the output to 500 characters
+                        if (textInput.Length > 500)
+                            textInput = textInput.Substring(0, 500);
+                        SpeechService.TextRecorded = textInput;
+                        Toast.MakeText(Android.App.Application.Context, textInput, ToastLength.Short).Show();
+                    }
+                    else
+                        Toast.MakeText(Android.App.Application.Context, "No speech was recognised", ToastLength.Short).Show();
+                    // change the text back on the button
+                    Toast.MakeText(Android.App.Application.Context, "Start Recording", ToastLength.Short).Show();
+                }
+                SpeechService.autoEvent.Set();
+            }
+
+            base.OnActivityResult(requestCode, resultVal, data);
+        }
+
+    }
+}
---
+++ SpeechText/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SpeechText
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGrabar_Clicked(object sender, EventArgs e)
+        {
+            string text = await DependencyService.Get<IDictation>().Dictate();
+            txtEditor.Text = text;
+        }
+    }
+}
